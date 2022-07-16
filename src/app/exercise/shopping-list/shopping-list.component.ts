a2b7a3db641b5639bdcbc451f5614d9505473add
Parkjoonil <106866130+Parkjoonil@users.noreply.github.com>
2022-07-17 01:42:13 +0900

    shopping list

--- src/app/exercise/shopping-list/shopping-list.component.ts
+++ src/app/exercise/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from './ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+
+  ingredients: Ingredient[] = [
+    new Ingredient('밀가루', 5),
+    new Ingredient('짜장소스', 10),
+    new Ingredient('양파', 5),
+    new Ingredient('돼지고기', 5),
+  ]
 
   constructor() { }
 
